7ddb39500f8efb1051364bf2760d2bb191800652
Geraint Jones <geraint@geraintjones.com>
2022-02-21 06:47:13 +0000

    Refactored for clarity.

--- DiamondKata/DiamondGenerator.cs
+++ DiamondKata/DiamondGenerator.cs
@@ -4,51 +4,52 @@ namespace DiamondKata;
 
 public class DiamondGenerator
 {
+        const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public StringCollection PrintDiamond(char input)
     {
-        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         var diamondLines = new StringCollection();
-        var inputIndex = alphabet.IndexOf(Char.ToUpper(input));
+        var inputIndex = Alphabet.IndexOf(Char.ToUpper(input));
 
         if (inputIndex < 0)
         {
             return new StringCollection() {"Input character is invalid - it must be a letter."};
         }
         var step = 1;
-        var lineIndex = 0;
+        var diaondLineIndex = 0;
         var letterIndex = 0;
-        while (lineIndex < 2 * (inputIndex + 1) - 1)
+        while (diaondLineIndex < 2 * (inputIndex + 1) - 1)
         {
-            if (lineIndex == inputIndex)
+            if (diaondLineIndex == inputIndex)
             {
                 step = -1;
             }
 
-            diamondLines.Add(new string(BuildLine(alphabet[letterIndex], inputIndex, letterIndex)));
+            diamondLines.Add(new string(BuildLine(inputIndex, letterIndex)));
 
             letterIndex += step;
-            lineIndex++;
+            diaondLineIndex++;
         }
 
         return diamondLines;
     }
 
-    char[] BuildLine(char lineCharacter, int inputIndex, int lineIndex)
+    char[] BuildLine(int inputIndex, int letterIndex)
     {
+        var lineCharacter = Alphabet[letterIndex];
         const char spaceChar = ' ';
 
-        var lineArray = new char[inputIndex + lineIndex + 1];
-        for (int i = 0; i <= inputIndex + lineIndex; i++)
+        var lineArray = new char[inputIndex + letterIndex + 1];
+        for (int i = 0; i <= inputIndex + letterIndex; i++)
         {
-            if (i == inputIndex - lineIndex)
+            if (i == inputIndex - letterIndex)
             {
                 lineArray[i] = lineCharacter;
                 continue;
             }
 
-            if (i == inputIndex + lineIndex)
+            if (i == inputIndex + letterIndex)
             {
                 lineArray[i] = lineCharacter;
                 continue;
